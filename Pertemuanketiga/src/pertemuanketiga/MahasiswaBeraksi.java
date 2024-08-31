3e78fbb3fb718a1fad7cf75776927cd27b2775ee
Bergascahyon <bergascahyonuswantoro281104@gmail.com>
2024-08-31 15:42:47 +0700

    first commit

---
+++ Pertemuanketiga/src/pertemuanketiga/MahasiswaBeraksi.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuanketiga;
+
+/**
+ *
+ * @author Dha Gas Gar
+ */
+public class MahasiswaBeraksi {
+    public static void main(String[] args) {
+        Mahasiswa mahasiswa = new Mahasiswa();
+        mahasiswa.membaca();
+        mahasiswa.nyontek();
+        mahasiswa.modifikasi();
+    }
+}
---
+++ Pertemuanketigasoaldua/src/pertemuanketigasoaldua/DemoNilai.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuanketigasoaldua;
+
+/**
+ *
+ * @author Dha Gas Gar
+ */
+public class DemoNilai {
+    public static void main(String[] args) {
+        Nilai nilai = new Nilai();
+        nilai.Nilai("123456", "John Doe", 85, 90, 80, 95);
+        nilai.CetakNilai();
+    }
+}
